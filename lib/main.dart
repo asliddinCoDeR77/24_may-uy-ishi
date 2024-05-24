78cbabdd0e699b0ddccd8dc0ae277a7520cd6e94
ASLIDDINCODER <147050428+ASLIDDINCODERn1@users.noreply.github.com>
2024-05-24 17:59:04 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:ux_2dars/views/screens/screen_1.dart';
-import 'package:ux_2dars/views/screens/uy_ishi_2.dart';
 import 'package:ux_2dars/views/screens/uy_ishi_3.dart';
 import 'package:ux_2dars/views/screens/uy_shi_1.dart';
 
